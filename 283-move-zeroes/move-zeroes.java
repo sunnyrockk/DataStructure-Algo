325ddfb49675664642c4ead1f4610ce9faa70ae0
sunny mall <sunnypratap859@gmail.com>
2026-01-15 12:33:05 +0530

    Time: 230 ms (5.03%) | Memory: 47.6 MB (88.54%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k= 0;
-        for(int i = 0; i< nums.length; i++){
-            if(nums[i] != 0){
-            nums[k] = nums[i];
-            k++;  
-            }
-        }
-        while(k < nums.length){
-            nums[k] = 0;
-            k++;
-        }
+       
+       int n = nums.length;
+       for(int i= 0 ; i< n-1; i++){
+           for(int j = 0; j<n-1-i; j++){
+               if(nums[j] ==0){
+                   int temp = nums[j];
+                   nums[j] = nums[j+1];
+                  nums[j+1] = temp;
+                   
+               }
+           }
+       
+}
  }
 }
